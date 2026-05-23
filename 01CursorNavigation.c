0faa753ebe492b962a323bb52065dfbaec665258
Vinay <iiith.vinay2025@gmail.com>
2026-05-23 17:15:58 +0530

    Add 01CursorNavigation.c - User can move around the letter 'X'.

---
+++ 01CursorNavigation.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+#include <ncurses.h>
+
+int main()
+{
+    // Initialize the screen, and allow the user to exit the same by pressing Ctrl + C!
+    initscr();
+    cbreak();
+    curs_set(0); // This hides the cursor!
+    keypad(stdscr, TRUE); // Enable the arrow keys!
+
+    int row = 0, column = 0; // Co-ordinates of the cursor!
+
+    while(true) {
+        clear(); // To remove the trailing X's!
+        mvprintw(row, column, "X");
+        refresh();
+
+        int userInput = getch();
+        if(userInput == KEY_UP) {
+            row--;
+        }
+        else if(userInput == KEY_RIGHT) {
+            column++;
+        }
+        else if(userInput == KEY_DOWN) {
+            row++;
+        }
+        else if(userInput == KEY_LEFT) {
+            column--;
+        }
+        else {
+            break;
+        }
+    }
+    endwin(); // Terminate the screen! 
+    return 0;
+}
